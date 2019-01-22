85c6c9928887be0df7f1e043dda2cb1113e9e160
Pedro R. Jiménez <ptpedroj@gmail.com>
2019-01-22 05:26:21 -0500

    ProofOfExistence - Added ProofOfExistence2.sol and Migration

--- proof-of-existence/migrations/2_deploy_contracts.js
+++ proof-of-existence/migrations/2_deploy_contracts.js
@@ -1,5 +1,7 @@
 var ProofOfExistence1 = artifacts.require("./ProofOfExistence1.sol");
+var ProofOfExistence2 = artifacts.require("./ProofOfExistence2.sol");
 
 module.exports = function(deployer) {
     deployer.deploy(ProofOfExistence1);
+    deployer.deploy(ProofOfExistence2);
 };
