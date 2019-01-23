c6f7653e12b6126335c797c688bbe11763a05806
Pedro R. Jiménez <ptpedroj@gmail.com>
2019-01-22 23:19:09 -0500

    ProofOfExistence - Added Rinkeby Config

--- proof-of-existence/truffle-config.js
+++ proof-of-existence/truffle-config.js
@@ -1,9 +1,19 @@
+const HDWalletProvider = require('truffle-hdwallet-provider');
+const infuraKey = "5867368ce4df44b8828d71d759fbaeed";
+const fs = require('fs');
+const mnemonic = fs.readFileSync(".secret").toString().trim();
+
 module.exports = {
   networks: {
       development: {
           host: 'localhost',
           port: 8545,
           network_id: '*'
+      },
+      rinkeby: {
+        provider: () => new HDWalletProvider(mnemonic, 'https://rinkeby.infura.io/v3/5867368ce4df44b8828d71d759fbaeed'),
+        network_id: 4,          // Rinkeby's id
+        gas: 5500000,        
       }
   }
 };
